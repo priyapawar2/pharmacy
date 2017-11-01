a2a63040c5ca7bef15ba0aab60f13e7dfbe88102
priyapawar2 <33164571+priyapawar2@users.noreply.github.com>
2017-11-01 12:00:00 -0700

    Update view.php

--- view.php
+++ view.php
@@ -15,7 +15,7 @@ exit();
 <!DOCTYPE html>
 <html>
 <head>
-<title><?php $user?> Pharmacy Sys</title>
+<title><?php $user?> Pharmacy System</title>
 <link rel="stylesheet" type="text/css" href="style/mystyle.css">
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" /> 
 <link rel="stylesheet" href="style/table.css" type="text/css" media="screen" /> 
@@ -27,7 +27,7 @@ exit();
 <body>
 <div id="content">
 <div id="header">
-<h1><a href="#"><img src="images/hd_logo.jpg"></a> Pharmacy Sys</h1></div>
+<h1><a href="#"><img src="images/hd_logo.jpg"></a> Pharmacy System</h1></div>
 <div id="left_column">
 <div id="button">
 		<ul>
@@ -164,7 +164,7 @@ exit();
     </div>  
 </div>
 </div>
-<div id="footer" align="Center"> Pharmacy Sys 2013. Copyright All Rights Reserved</div>
+<div id="footer" align="Center"> Pharmacy System 2016. Copyright All Rights Reserved</div>
 </div>
 </body>
 </html>
