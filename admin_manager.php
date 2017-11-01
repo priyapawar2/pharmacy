9d1b70384fc252cae46491696ea9e98863c99dfe
priyapawar2 <33164571+priyapawar2@users.noreply.github.com>
2017-11-01 11:37:47 -0700

    Update admin_manager.php

--- admin_manager.php
+++ admin_manager.php
@@ -33,7 +33,7 @@ $message1="<font color=red>Registration Failed, Try again</font>";
 <!DOCTYPE html>
 <html>
 <head>
-<title><?php echo $username;?> - Pharmacy Sys</title>
+<title><?php echo $username;?> - Pharmacy System</title>
 <link rel="stylesheet" type="text/css" href="style/mystyle.css">
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" /> 
 <link rel="stylesheet" href="style/table.css" type="text/css" media="screen" /> 
@@ -98,7 +98,7 @@ return false;
 <body>
 <div id="content">
 <div id="header">
-<h1><a href="#"><img src="images/hd_logo.jpg"></a> Pharmacy Sys</h1></div>
+<h1><a href="#"><img src="images/hd_logo.jpg"></a> Pharmacy System</h1></div>
 <div id="left_column">
 <div id="button">
 <ul>
@@ -191,7 +191,7 @@ return false;
 </div>
  
 </div>
-<div id="footer" align="Center"> Pharmacy Sys 2013. Copyright All Rights Reserved</div>
+<div id="footer" align="Center"> Pharmacy System 2016. Copyright All Rights Reserved</div>
 </div>
 </body>
 </html>
